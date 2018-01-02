aba36a1542dfbaee31757943df8d47319b92b7d9
riskinputra <riski.n.putra@gmail.com>
2018-01-02 16:29:18 +0700

    multer success

---
+++ server/controllers/houses.js
@@ -0,0 +1,51 @@
+const House = require('../models/house')
+const images = require('../helpers/images')
+
+class HouseController {
+  static findAll(req, res) {
+    House.find()
+    .then( result => {
+      res.ststus(200).json({
+        message: 'House List',
+        data: result
+      })
+    })
+    .catch(err => {
+      res.status(500).send(err)
+    })
+  }
+
+  static addHouse(req, res) {
+
+    let dataHouse = new House({
+      contactName: req.body.contactName,
+      contactNumber: req.body.contactNumber,
+      title: req.body.title,
+      address: req.body.address,
+      latitude: null,
+      longtitude: null,
+      price: req.body.price,
+      image: req.file.cloudStoragePublicUrl,
+      kamarTidur: req.body.kamarTidur,
+      kamarMandi: req.body.kamarMandi,
+      bangunan: req.body.bangunan,
+      tanah: req.body.tanah,
+      lantai: req.body.lantai,
+      watt: req.body.watt,
+      thnBangun: req.body.thnBangun
+    })
+    dataHouse.save()
+    .then(result => {
+      res.status(200).json({
+        message: 'Success To Input',
+        data: result
+      })
+    })
+    .catch(err => {
+      res.status(500).send(err)
+    })
+  }
+
+}
+
+module.exports = HouseController
---
+++ server/routes/houses.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const HouseController = require('../controllers/houses')
+const images = require('../helpers/images')
+
+module.exports = router 
+  .get('/', HouseController.findAll)
+  .post('/', images.multer.single('image'), images.sendUploadToGCS, HouseController.addHouse)
